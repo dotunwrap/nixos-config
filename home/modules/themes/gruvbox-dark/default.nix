854603a65a84efc3f2e2fda314b6a6f32a0dd234
Gabby Simpson <gabby@dotunwrap.dev>
2025-11-06 23:51:16 -0500

    feat: Properly set bg in stylix theme

--- home/modules/themes/gruvbox-dark/default.nix
+++ home/modules/themes/gruvbox-dark/default.nix
@@ -1,19 +1,24 @@
-_:
-{
+_: {
   config,
   lib,
   pkgs,
   ...
-}:
-let
+}: let
   cfg = config.themes.gruvbox-dark;
-  wallpaper = ../../../../assets/wallpapers/gruvbox_pixels.png;
-in
-{
+  inputImage = ./../../../../assets/wallpapers/gruvbox_pixels.png;
+  brightness = "-30";
+  contrast = "0";
+  fillColor = "black";
+  outputImage = pkgs.runCommand "dimmed-background.png" {} ''
+    ${lib.getExe' pkgs.imagemagick "convert"} "${inputImage}" -brightness-contrast ${brightness},${contrast} -fill ${fillColor} $out
+  '';
+in {
   config = lib.mkIf cfg.enable {
     stylix = {
-      image = wallpaper;
+      image = outputImage;
       base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark.yaml";
     };
+
+    home.file.".background-image".source = lib.mkIf config.bundles.dwm.enable outputImage;
   };
 }
