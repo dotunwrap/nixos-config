36a400791deca01fe818a20724f148517e297ef0
Gabby Simpson <gabby@dotunwrap.dev>
2025-11-07 00:58:41 -0500

    fix: Apply image option properly

--- home/modules/themes/default.nix
+++ home/modules/themes/default.nix
@@ -9,6 +9,7 @@ with lib;
 {
   options.themes = {
     enable = mkEnableOption "Enable stylix themeing";
+    image = mkOption { };
     gruvbox-dark.enable = mkEnableOption "Enable gruvbox-dark";
     flare.enable = mkEnableOption "Enable flare";
   };
@@ -16,6 +17,8 @@ with lib;
   config = mkIf config.themes.enable {
     stylix = {
       enable = true;
+      image = config.themes.image;
+      polarity = "dark";
       fonts = {
         # serif = { };
         # sansSerif = { };
@@ -26,5 +29,17 @@ with lib;
         # emoji = { };
       };
     };
+
+    home.file.".background-image".source = lib.mkIf config.bundles.dwm.enable config.themes.image;
+    xresources.properties =
+      with config.lib.stylix.colors.withHashtag;
+      lib.mkIf config.bundles.dwm.enable {
+        "dwm.normbordercolor" = base00;
+        "dwm.normbgcolor" = base00;
+        "dwm.normfgcolor" = base0D;
+        "dwm.selbordercolor" = base03;
+        "dwm.selbgcolor" = base0D;
+        "dwm.selfgcolor" = base00;
+      };
   };
 }
