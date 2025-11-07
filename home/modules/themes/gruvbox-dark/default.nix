f3e73cb759f39c3018de9a6692ca9ecac43b78a5
Gabby Simpson <gabby@dotunwrap.dev>
2025-11-07 00:45:46 -0500

    chore: Format

--- home/modules/themes/gruvbox-dark/default.nix
+++ home/modules/themes/gruvbox-dark/default.nix
@@ -1,18 +1,21 @@
-_: {
+_:
+{
   config,
   lib,
   pkgs,
   ...
-}: let
+}:
+let
   cfg = config.themes.gruvbox-dark;
   inputImage = ./../../../../assets/wallpapers/gruvbox_pixels.png;
   brightness = "-15";
   contrast = "0";
   fillColor = "black";
-  outputImage = pkgs.runCommand "dimmed-background.png" {} ''
+  outputImage = pkgs.runCommand "dimmed-background.png" { } ''
     ${lib.getExe' pkgs.imagemagick "convert"} "${inputImage}" -brightness-contrast ${brightness},${contrast} -fill ${fillColor} $out
   '';
-in {
+in
+{
   config = lib.mkIf cfg.enable {
     stylix = {
       image = outputImage;
@@ -20,7 +23,8 @@ in {
     };
 
     home.file.".background-image".source = lib.mkIf config.bundles.dwm.enable outputImage;
-    xresources.properties = with config.lib.stylix.colors.withHashtag;
+    xresources.properties =
+      with config.lib.stylix.colors.withHashtag;
       lib.mkIf config.bundles.dwm.enable {
         "dwm.normbordercolor" = base00;
         "dwm.normbgcolor" = base00;
