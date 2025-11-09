f9da6cfe56f59f55db9701e6023d98df031fd848
Gabby Simpson <gabby@dotunwrap.dev>
2025-11-09 00:47:39 -0500

    fix: Add required `open` definition for nvidia

--- nixos/configurations/sekai.nix
+++ nixos/configurations/sekai.nix
@@ -17,6 +17,7 @@ _:
       enable = true;
     };
     nvidia = {
+      open = false;
       modesetting.enable = true;
       nvidiaSettings = true;
       package = config.boot.kernelPackages.nvidiaPackages.stable;
