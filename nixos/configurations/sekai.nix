3afeba6a0cb91ea075e12c83b439f3ff57addce3
Gabby Simpson <gabby@dotunwrap.dev>
2025-11-09 00:43:08 -0500

    fix: Remove non-existant hardware.nvidia.enable option

--- nixos/configurations/sekai.nix
+++ nixos/configurations/sekai.nix
@@ -17,7 +17,6 @@ _:
       enable = true;
     };
     nvidia = {
-      enable = true;
       modesetting.enable = true;
       nvidiaSettings = true;
       package = config.boot.kernelPackages.nvidiaPackages.stable;
