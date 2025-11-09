cc3d53529151d76a8fb57225541f053c5afa53c2
Gabby Simpson <gabby@dotunwrap.dev>
2025-11-09 07:15:00 -0500

    feat: Configure zsh

--- home/modules/programs/shells/zsh/default.nix
+++ home/modules/programs/shells/zsh/default.nix
@@ -11,10 +11,27 @@ in
   config = lib.mkIf cfg.enable {
     programs.zsh = {
       autocd = lib.mkDefault true;
-      defaultKeymap = lib.mkDefault "vicmd";
-      autosuggestion.enable = lib.mkDefault true;
-      syntaxHighlighting.enable = lib.mkDefault true;
       shellAliases = { } // import ../shellAliases.nix;
+
+      antidote = {
+        enable = true;
+        plugins = [
+          "jeffreytse/zsh-vi-mode"
+
+          "mattmc3/ez-compinit"
+
+          "getantidote/use-omz"
+          "ohmyzsh/ohmyzsh path:lib"
+          "ohmyzsh/ohmyzsh path:plugins/colored-man-pages"
+          "ohmyzsh/ohmyzsh path:plugins/direnv"
+          "ohmyzsh/ohmyzsh path:plugins/sudo"
+
+          "mattmc3/zfunctions"
+          "zsh-users/zsh-autosuggestions"
+          "zdharma-continuum/fast-syntax-highlighting kind:defer"
+          "zsh-users/zsh-history-substring-search"
+        ];
+      };
     };
   };
 }
