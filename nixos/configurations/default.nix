8236969dd784d6b9a8c9debdea5a7cc5d1b0d7ad
Nia Williams <107441585+nyoshe@users.noreply.github.com>
2026-01-17 19:10:49 -0500

    feat(mokusei): Add mokusei system (#31)

--- nixos/configurations/default.nix
+++ nixos/configurations/default.nix
@@ -4,4 +4,5 @@ _:
   dotunwrap.nixosConfigurations.sekai.system = "x86_64-linux";
   dotunwrap.nixosConfigurations.suigin.system = "x86_64-linux";
   dotunwrap.nixosConfigurations.europa.system = "x86_64-linux";
+  dotunwrap.nixosConfigurations.mokusei.system = "x86_64-linux";
 }
