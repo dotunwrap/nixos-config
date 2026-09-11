2fa01d6b7e3a65614a3452dbadd1226c84256e14
Gabby Simpson <gabby@dotunwrap.dev>
2026-09-10 22:28:28 -0400

    fix: Use config instead of settings for direnv

--- home/modules/programs/direnv/default.nix
+++ home/modules/programs/direnv/default.nix
@@ -11,7 +11,8 @@ in
   config = lib.mkIf cfg.enable {
     programs.direnv = {
       nix-direnv.enable = lib.mkDefault true;
-      settings.global.hide_env_diff = lib.mkDefault true;
+      silent = lib.mkDefault true;
+      config.global.hide_env_diff = lib.mkDefault true;
     };
   };
 }
