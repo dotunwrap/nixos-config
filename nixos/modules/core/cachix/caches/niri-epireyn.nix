17a9823fd23b3a80ee31cb37da2b8653b6ce9ed5
Nia Williams <niayoshe@gmail.com>
2026-09-20 03:09:26 -0400

    chore: Add niri-flake binary cache

---
+++ nixos/modules/core/cachix/caches/niri-epireyn.nix
@@ -0,0 +1,6 @@
+_: {
+  nix.settings = {
+    substituters = [ "https://niri-epireyn.cachix.org" ];
+    trusted-public-keys = [ "niri-epireyn.cachix.org-1:tlVyFN7CtsDT+ZcLPS+ekFWeT1X6X4OqvWqbBMyIzFA=" ];
+  };
+}
